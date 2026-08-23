654327804cccd1f3976dd02171a45a3583db43ec
Kevin Broughan <kevbroughan@gmail.com>
2026-08-23 20:12:13 +1200

    updated Eunice.jl to add include code

--- src/Eunice.jl
+++ src/Eunice.jl
@@ -1,5 +1,41 @@
 module Eunice
 
-greet() = print("Hello World!")
+const EUNICE_DIR = @__DIR__
 
+# using ...
+
+# core:
+include("./support_functions.jl")
+include("./utils.jl")
+include("./Eunice_types.jl")
+
+# EuniceCairo
+include("./Render/EuniceCairo.jl")
+include("./Render/render_vegetation.jl"
+include("./Render/render_orography.jl"
+include("./Render/render_mask.jl")
+include("./Render/render_scalar_field.jl")
+include("./Render/render_scalar_levels.jl")
+
+# EuniceGL
+include("./Show/EuniceGL.jl")
+include("./Show/show_vegetation.jl")
+include("./Show/show_orography.jl")
+include("./Show/show_scalar_field.jl")
+include("./Show/show_scalar_levels.jl")
+include("./Show/show_vector_field.jl")
+include("./Show/show_vector_levels.jl")
+
+# Tools
+include("./Tools/eds_small_tools.jl")
+include("./Tools/eds_process_2d.jl")
+include("./Tools/eds_collate.jl")
+include("./Tools/eds_extract.jl")
+include("./Tools/eds_thin_deflate.jl")
+include("./Tools/eds_extend_time_series.jl")
+include("./Tools/eds_generate_type.jl")
+
+# export ...
+
+println("Eunice loaded")
 end # module Eunice
